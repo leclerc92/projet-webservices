780d1e8f39c1913fceb74b6829c921d3057561ce
leclerc92 <leclerc92@gmail.com>
2026-01-19 10:34:48 +0100

    ajout d'un frontend

--- WorkflowService/src/main/java/com/micheldev/workflowservice/controller/WorkflowController.java
+++ WorkflowService/src/main/java/com/micheldev/workflowservice/controller/WorkflowController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/workflow")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class WorkflowController {
 
